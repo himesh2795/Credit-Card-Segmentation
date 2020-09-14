34fe139240635161711e43323893bf5cdb41d473
himesh2795 <48667682+himesh2795@users.noreply.github.com>
2020-09-15 00:02:37 +0530

    Update card_segment.py

--- card_segment.py
+++ card_segment.py
@@ -7,6 +7,10 @@ import seaborn as sns
 # from sklearn.decomposition import PCA, KernelPCA
 from sklearn.cluster import KMeans
 import scipy.cluster.hierarchy as sch
+from factor_analyzer import FactorAnalyzer
+from factor_analyzer.factor_analyzer import calculate_bartlett_sphericity, calculate_kmo
+from sklearn.decomposition import FactorAnalysis
+
 
 # Load data
 card_df = pd.read_csv("credit-card-data.csv")
@@ -21,7 +25,8 @@ missing_val = pd.DataFrame(card_df.isnull().sum(), columns = ['Count'])
 missing_val['Percentage'] = (missing_val.loc[:,'Count']*100)/len(card_df)
 
 # Remove missing values
-card_df.drop((card_df[card_df['CREDIT_LIMIT'].isnull() | card_df['MINIMUM_PAYMENTS'].isnull()]).index, inplace=True)
+# card_df.drop((card_df[card_df['CREDIT_LIMIT'].isnull() | card_df['MINIMUM_PAYMENTS'].isnull()]).index, inplace=True)
+card_df.dropna(inplace=True)
 print(f"DataLoss After removing missing values : {total_rows-len(card_df)}")
 
 card_df.info()
@@ -59,6 +64,49 @@ card_df.CASH_ADVANCE.mean()
 ## 1.6 - limit usage (balance to credit limit ratio)
 
 ## 1.7 - payments to minimum payments ratio 
+
+
+########## Dimensionality Reduction ##########
+## Factor Analysis
+
+# Bartlett’s test of sphericity checks whether or not the observed variables intercorrelate at all using the observed correlation matrix against the identity matrix. If the test found statistically insignificant, you should not employ a factor analysis
+chi_square_value,p_value=calculate_bartlett_sphericity(card_df)
+print(chi_square_value, p_value)
+
+#In this Bartlett ’s test, the p-value is 0. The test was statistically significant, indicating that the observed correlation matrix is not an identity matrix.
+
+# Kaiser-Meyer-Olkin (KMO) Test measures the suitability of data for factor analysis. It determines the adequacy for each observed variable and for the complete model. KMO estimates the proportion of variance among all the observed variable. Lower proportion id more suitable for factor analysis. KMO values range between 0 and 1. Value of KMO less than 0.6 is considered inadequate.
+kmo_all,kmo_model=calculate_kmo(card_df)
+# Here kmo_model value is 0.64, so it is adequate.
+
+## Choosing the Number of Factors
+# Create factor analysis object and perform factor analysis
+fa = FactorAnalyzer()
+fa.set_params(n_factors=25, rotation=None)
+fa.fit(card_df)
+# Check Eigenvalues
+ev, v = fa.get_eigenvalues()
+ev
+
+# Create scree plot using matplotlib
+plt.scatter(range(1,card_df.shape[1]+1),ev)
+plt.plot(range(1,card_df.shape[1]+1),ev)
+plt.title('Scree Plot')
+plt.xlabel('Factors')
+plt.ylabel('Eigenvalue')
+plt.grid()
+plt.show()
+
+# The scree plot method draws a straight line for each factor and its eigenvalues. Number eigenvalues greater than one considered as the number of factors.
+# Here, you can see only for 5-factors eigenvalues are greater than one. It means we need to choose only 5 factors (or unobserved variables).
+fa = FactorAnalyzer()
+fa.set_params(n_factors=5, rotation="varimax")
+fa.fit(card_df)
+
+loadings = fa.loadings_
+
+
+
 
 
 ######## K-Means Clustering ###########
